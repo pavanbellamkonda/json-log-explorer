d2b8f4ac1db516c3f48a9e1515d12f7ea61733a4
Pavan Bellamkonda <pavanbellamkonda98@gmail.com>
2023-03-18 16:56:24 +0530

    basic data display

--- worker.js
+++ worker.js
@@ -1,3 +1,24 @@
 onmessage = function(e) {
     console.log('Worker: Message received from main script', JSON.stringify(e.data));
+    console.log(e.data, splitJSON(e.data.text))
+
+    return postMessage(splitJSON(e.data.text));
+}
+
+
+function splitJSON(fullText) {
+    const stack = [];
+    /** @type number[] */
+    const pos = [];
+    let pop = false;
+    for (let i=0; i<fullText.length; i++) {
+        pop = false;
+        if (['{', '['].includes(fullText[i])) stack.push(fullText[i]);
+        if (['}', ']'].includes(fullText[i])) {
+            pop = true;
+            stack.pop();
+        }
+        if (stack.length === 0 && pop) pos.push(i);
+    }
+    return pos.map((p,i) => fullText.substring((pos[i - 1] + 1)||0, p+1)).map(s => JSON.parse(s));
 }
